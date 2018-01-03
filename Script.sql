85d3ac5551cb23d1018a17a1568529c7d6faf29f
Haja <hajaedn@gmail.com>
2018-01-03 16:13:56 +0100

    Create table

---
+++ Script.sql
@@ -0,0 +1,13 @@
+CREATE
+	TABLE
+		users(
+			usr_id INT AUTO_INCREMENT PRIMARY KEY,
+			usr_login VARCHAR(30),
+			usr_pwd VARCHAR(20),
+			usr_name VARCHAR(50),
+			usr_right VARCHAR(20),
+			usr_create DATE,
+			usr_enable INT(1)
+		);
+
+
