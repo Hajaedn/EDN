dabda992901b5b8c5f29d2d1085df8cb34135ed7
Matthieu Besson <bmatthieu.edn@gmail.com>
2018-01-04 14:45:42 +0100

    methode saveInDatabase : création

--- User.php
+++ User.php
@@ -1,16 +1,93 @@
 <?php
+require_once 'ma_lib.php';
 
-class User
+abstract class DbEntity
+{
+    /** @return string */
+    abstract public function getDbPrimaryKey();
+    /** @return string */
+    abstract public function getDbTableName();
+    /** @return array */
+    abstract public function getDbColumnsMapping();
+
+    /**
+     * @param PDO $pdo
+     * @throws Exception
+     */
+    public function saveInDatabase(PDO $pdo){
+        //user infos are not set or empty
+        if(empty($this->_login) || empty($this->_password) || empty($this->_name) || empty($this->_rights) || empty($this->_creationDate) || empty($this->_enable)){
+            throw new InvalidArgumentException("At least one parameter is not set or empty");
+        }
+
+        if(empty($this->_id)) {
+            // Liste des Utilisateurs "actifs"
+
+            $sql = 'INSERT INTO ' . $this->getDbTableName() . '(';
+
+            $sql .= implode(',', array_values($this->getDbColumnsMapping()));
+
+            $sql .= ') VALUES (';
+            $sql .= implode(',',
+                array_map(
+                    function($v) {
+                        return ':' . $v;
+                    }, $this->getDbColumnsMapping()
+                )
+            );
+            $sql .= ')';
+
+            $prep = $pdo->prepare($sql);
+
+            foreach($this->getDbColumnsMapping() as $attrName => $dbName) {
+                $prep->bindParam($dbName, $this->$attrName, PDO::PARAM_STR);
+            }
+
+            $prep->execute();
+
+            $this->_id = $pdo->lastInsertId();
+        } else {
+
+        }
+    }
+}
+
+class User extends DbEntity
 {
     const RIGHTS_USER = 'user';
     const RIGHTS_ADMIN = 'admin';
 
-    private $_id;
+    private $_dbPrimaryKey;
     private $_login;
     private $_password;
     private $_rights;
+    private $_creationDate;
+    private $_name;
+    private $_enable;
 
     private static $_toto = 'titi';
+
+    public function getDbTableName()
+    {
+        return 'users';
+    }
+
+    public function getDbColumnsMapping()
+    {
+        return [
+            '_login' => 'usr_login',
+            '_password' => 'usr_login',
+            '_name' => 'usr_name',
+            '_right' => 'usr_right',
+            '_creationDate' => 'usr_create',
+            '_enable' => 'usr_enable'
+        ];
+    }
+
+    public function getDbPrimaryKey()
+    {
+        return $this->_dbPrimaryKey;
+    }
 
     /**
      * @return array
@@ -28,20 +105,8 @@ class User
         ];
     }
 
-    /**
-     * @param int $id
-     * @return User
-     */
-    public function setId($id)
-    {
-        //contrôle de l'argument
-        if(!is_int($id)) {
-            throw new InvalidArgumentException('Id is not an integer');
-        }
+    public function setUserInfo(){
 
-        $this->_id = $id;
-
-        return $this;
     }
 
     /**
@@ -60,8 +125,27 @@ class User
 
         return $this;
     }
+
+    public function connect(User $user){
+        $_SESSION['connected_user'] = $user->getId();
+    }
+
+    public function disconnect() {
+        unset($_SESSION['connected_user']);
+    }
+
 }
 
-$user = new User();
+/*
+ * Notes :
+ * 1) Chaînage des méthodes possible grâce à "return $this;" en fin de méthode
+ *
+ *      $user = new User();
+ *      $user->setId(1)->setRights('admin');
+ *
+ */
 
-$user->setId(1)->setRights('admin');//chaînage des méthodes possible grâce à "return $this;" en fin de méthode
+
+
+$user = new User();
+$user->saveInDatabase($pdo);
