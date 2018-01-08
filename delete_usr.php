308ccfa1b6e9c67844fee2144270b1260745ebe9
Matthieu Besson <bmatthieu.edn@gmail.com>
2018-01-08 10:39:17 +0100

    prépa à l'intégration de la fonction de suppression de User.php dans le code existant

--- delete_usr.php
+++ delete_usr.php
@@ -18,19 +18,25 @@ if (empty($_SESSION['login'])) {
     header("Location: index.php");
 }
 
-
 try {
-    $pdo = new pdo($dsn, $user, $password, $opt);
-
-    // Liste des Utilisateurs "actifs"
-
-    $query = 'DELETE FROM users WHERE usr_id =:usr_id';
-    $prep = $pdo->prepare($query);
-    $prep->bindValue(':usr_id', $key, PDO::PARAM_STR);
-    $prep->execute();
-} catch (PDOException $e) {
-    die('Erreur : ' .$e->getMessage());
+    $user->deleteInDataBase($pdo);
+} catch (Exception $e) {
+    die($e->getMessage());
 }
+
+//
+//try {
+//    $pdo = new pdo($dsn, $user, $password, $opt);
+//
+//    // Liste des Utilisateurs "actifs"
+//
+//    $query = 'DELETE FROM users WHERE usr_id =:usr_id';
+//    $prep = $pdo->prepare($query);
+//    $prep->bindValue(':usr_id', $key, PDO::PARAM_STR);
+//    $prep->execute();
+//} catch (PDOException $e) {
+//    die('Erreur : ' .$e->getMessage());
+//}
 header("Location: usr_list.php");
 ?>
 
