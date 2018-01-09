a790ce1a62c575a270b0c3200893392bf987daf9
Matthieu Besson <bmatthieu.edn@gmail.com>
2018-01-09 15:07:34 +0100

    use of User class in fiche_view

--- usr_fiche_view.php
+++ usr_fiche_view.php
@@ -22,32 +22,32 @@ if (empty($_SESSION['login'])) {
 
 $pdo = new pdo($dsn, $user, $password, $opt);
 
-// Liste des Utilisateurs "actifs"
+try {
+    $userToSee = User::getFromDataBase($pdo, $key);
+} catch (Exception $e) {
+    //pas connecté ou problème de connexion bdd
+    die($e->getMessage());
+}
 
-$query = 'SELECT * FROM users WHERE usr_id =:usr_id';
-$prep = $pdo->prepare($query);
-$prep->bindValue(':usr_id', $key, PDO::PARAM_STR);
-$prep->execute();
-$result = $prep->fetch();
 $enable = 'Off';
-if ($result['usr_enable']) {
+if ($userToSee->getEnable()) {
     $enable = 'Actif';
 };
 $type_prf = 'Utilisateur';
-if ($result['usr_right']==User::RIGHTS_ADMIN) {
+if ($userToSee->getRights() == User::RIGHTS_ADMIN) {
     $type_prf = 'Administrateur';
 };
 ?>
 
 <fieldset>
     <legend>Informations d'identification</legend>
-    <b>Nom :  </b><a><?php echo $result['usr_name']; ?></a> <br><br>
-    <b>Profil : </b><a><?php echo $result['usr_login'] ?></a><br><br>
-    <b>Mot de passe : </b><a><?php echo $result['usr_pwd']; ?></a>
+    <b>Nom :  </b><a><?php echo $userToSee->getName(); ?></a> <br><br>
+    <b>Profil : </b><a><?php echo $userToSee->getLogin() ?></a><br><br>
+    <b>Mot de passe : </b><a><?php echo $userToSee->getPassword(); ?></a>
 </fieldset><br>
 <fieldset>
     <legend>Informations annexes</legend>
-    <b>Date d'inscription : </b><a><?php echo strftime($result['usr_create']); ?></a><br><br>
+    <b>Date d'inscription : </b><a><?php echo strftime($userToSee->getCreationDate()); ?></a><br><br>
     <b>Type de profil : </b><a><?php echo $type_prf; ?></a><br><br>
     <b>Profil actif : </b><a><?php echo $enable; ?></a>
 </fieldset><br>
@@ -58,7 +58,7 @@ if ($result['usr_right']==User::RIGHTS_ADMIN) {
 ?>
     <form method="get" action="delete_usr.php">
         <p><br>
-            <input type="hidden" name="id" value="<?= $key ?>">
+            <input type="hidden" name="id" value="<?= $userToSee->getId() ?>">
             <br><input type="submit" value="Delete">
         </p>
 
