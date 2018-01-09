9c338889b89c23a5c9b3673bf9431ac483c4559d
Matthieu Besson <bmatthieu.edn@gmail.com>
2018-01-09 16:10:55 +0100

    utilisation de la table bootstrap

--- usr_list.php
+++ usr_list.php
@@ -8,6 +8,7 @@ $user = User::getFromDataBase($pdo, $_SESSION['id']);
 if (empty($user)) {header("Location: index.php");}
 require_once 'header.php';
 ?>
+
 <h1>Liste des utilisateurs</h1>
 
 <a>Vous êtes : </a><?php echo $_SESSION['name'] ?>
@@ -27,56 +28,74 @@ $arrAll=$prep->fetchAll();
 <p>Créer un utilisateur <a href="usr_new.php">Création</a><br></p>
 
 <!-- Tableau : Liste des utilisateurs -->
-<table border=2 cellspacing="2" width="80%" align="center">
-    <!-- Ligne titre -->
-    <tr>
-        <th width="15%"> Utilisateur </th>
-        <th width="15%"> Profil </th>
-        <th width="15%"> Mot de passe </th>
-        <th width="15%"> Droits </th>
-        <th width="15%"> Date de création </th>
-        <th width="10%"> Actif O/N </th>
-        <th width="15%"> Action </th>
-    </tr>
-<?php
-    foreach($arrAll as $ligne) {
-        $name = $ligne['usr_name'];
-        $id = $ligne['usr_id'];
-        $login = $ligne['usr_login'];
-        $pwd = $ligne['usr_pwd'];
-        $right = $ligne['usr_right'];
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <table class="table">
+                <!-- Ligne titre -->
+                <tr>
+                    <th> Utilisateur </th>
+                    <th> Profil </th>
+                    <th> Mot de passe </th>
+                    <th> Droits </th>
+                    <th> Date de création </th>
+                    <th> Actif O/N </th>
+                    <th> Action </th>
+                </tr>
+                <?php
+                foreach($arrAll as $ligne) {
+                    $name = $ligne['usr_name'];
+                    $id = $ligne['usr_id'];
+                    $login = $ligne['usr_login'];
+                    $pwd = $ligne['usr_pwd'];
+                    $right = $ligne['usr_right'];
 
 
-        $create = $ligne['usr_create'];
+                    $create = $ligne['usr_create'];
 
-        //change date format
-        $myDateTime = DateTime::createFromFormat('Y-m-d', $create);
-        $create = $myDateTime->format('d-m-Y');
+                    //change date format
+                    $myDateTime = DateTime::createFromFormat('Y-m-d', $create);
+                    $create = $myDateTime->format('d-m-Y');
 
-        $enable = 'off';
-        if ($ligne['usr_enable']) {
-            $enable = 'actif';
-        };
-    ?>
-    <tr>
-        <td><?= $name ?></td>
-        <td><?= $login ?></td>
-        <td><?= $pwd ?></td>
-        <td align='center'><?= $right ?></td>
-        <td align='center'><?= $create ?></td>
-        <td align='center'><?= $enable ?></td>
-        <td><a href='usr_fiche_view.php?id=<?= $id ?>&action=voir'>Voir</a>
-            <?php if (($_SESSION['sess_droits'] == 'admin') || ($_SESSION['id']==$id)){ ?>
-            <a href="usr_fiche_edit.php?nom=<?= $id ?>">Edit</a>
-            <a href='usr_fiche_view.php?id=<?= $id ?>&action=suppr'>Delete</a>
-                <?php } ?>
-        </td>
-    </tr>
+                    $enable = 'off';
+                    if ($ligne['usr_enable']) {
+                        $enable = 'actif';
+                    };
+                    ?>
+                    <tr>
+                        <td><?= $name ?></td>
+                        <td><?= $login ?></td>
+                        <td><?= $pwd ?></td>
+                        <td align='center'><?= $right ?></td>
+                        <td align='center'><?= $create ?></td>
+                        <td align='center'><?= $enable ?></td>
+                        <td>
+                            <div class="dropdown show">
+                                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 
-<?php
-    }
-?>
-</table>
+                                </a>
+
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                    <a class="dropdown-item" href='usr_fiche_view.php?id=<?= $id ?>&action=voir'>Voir</a>
+                                    <?php if (($_SESSION['sess_droits'] == 'admin') || ($_SESSION['id']==$id)){ ?>
+                                        <a class="dropdown-item" href="usr_fiche_edit.php?nom=<?= $id ?>">Edit</a>
+                                        <a class="dropdown-item" href='usr_fiche_view.php?id=<?= $id ?>&action=suppr'>Delete</a>
+                                    <?php } ?>
+                                </div>
+                            </div>
+
+
+                        </td>
+                    </tr>
+
+                    <?php
+                }
+                ?>
+            </table>
+        </div>
+    </div>
+</div>
+
 
 <form method="post" action="disconnect.php">
     <p>
@@ -92,4 +111,7 @@ $prep->closeCursor();
 $prep=NULL;
 die;
 ?>
+
 </body>
+
+
