60a0297220475b00595b89e29981f23c83db3a21
Matthieu Besson <bmatthieu.edn@gmail.com>
2018-01-11 14:29:11 +0100

    update view usr_fiche_edit

--- templates/usr_fiche_edit.php
+++ templates/usr_fiche_edit.php
@@ -1,58 +1,35 @@
 <?php
-//require_once 'ma_lib.php';
-//session_start();
-//
-//$pdo = new pdo($dsn, $user, $password, $opt);
-//$user = User::getFromDataBase($pdo, $_SESSION['id']);
-//if (empty($user)) {header("Location: index.php");}
-//require_once 'header.php';
-//?>
+/**
+ * @var User $userSelected
+ * @var string $modAct
+ */
+?>
 
 <div class="jumbotron jumbotron-fluid">
     <div class="container">
         <h1>Fiche utilisateur</h1>
-
         <!-- Saisie des infos pour un utilisateur existant -->
-        <!-- Première ligne avec du code PHP -->
-        <?php
-
-        $key=$userToSee->getId();
-
-        try {
-            $userToEdit = User::getFromDataBase($pdo, $key);
-
-        } catch (Exception $e) {
-            //utilisateur non connecté
-            die($e->getMessage());
-        }
-
-
-        if (CheckRight($_SESSION['id'], $key)==false) {
-            throw new Exception('Pas les droits');
-        }
-
-        ?>
     </div>
 </div>
 
 
 <div class="container">
 <form method="post" action="update_usr.php">
-    <input type="hidden" name="usr_id" value="<?php echo $userToEdit->getId(); ?>" />
+    <input type="hidden" name="usr_id" value="<?php echo $userSelected->getId(); ?>" />
 
     <legend>Informations d'identification</legend>
     <div class="form-group">
         <label for="Nom">Nom</label>
-        <input class="form-control" id="Nom" type="text" name="usr_name" value="<?php echo $userToEdit->getName(); ?>"/>
+        <input class="form-control" id="Nom" type="text" name="usr_name" value="<?php echo $userSelected->getName(); ?>"/>
     </div>
     <div class="form-row">
         <div class="form-group col-md-6">
             <label for="Profil">Profil</label>
-            <input class="form-control" id="Profil" type="text" name="usr_login" value="<?php echo $userToEdit->getLogin() ?>" <?php echo $modAct!=='edit' ? 'disabled' : '' ?>/>
+            <input class="form-control" id="Profil" type="text" name="usr_login" value="<?php echo $userSelected->getLogin() ?>" <?php echo $modAct!=='edit' ? 'disabled' : '' ?>/>
         </div>
         <div class="form-group col-md-6">
             <label for="Pass">Mot de Passe</label>
-            <input type="text" class="form-control" id="Pass" name="usr_pwd" value="<?php echo $userToEdit->getPassword(); ?>"/>
+            <input type="text" class="form-control" id="Pass" name="usr_pwd" value="<?php echo $userSelected->getPassword(); ?>"/>
         </div>
     </div>
 
@@ -61,7 +38,7 @@
     <div class="form-row">
         <div class="form-group col-md-4">
             <label for="Date">Date d'inscription :</label>
-            <input type="date" class="form-control" id="Date" name="usr_create" value="<?php echo $userToEdit->getCreationDate(); ?>"/>
+            <input type="date" class="form-control" id="Date" name="usr_create" value="<?php echo $userSelected->getCreationDate(); ?>"/>
 
         </div>
         <div class="form-group col-md-4">
@@ -72,7 +49,7 @@
 
                 <?php
                 foreach(User::getRightsValues() as $rightValue){
-                        $mustCheckRadio = ($rightValue == $userToEdit->getRights());
+                        $mustCheckRadio = ($rightValue == $userSelected->getRights());
                         echo '<input class="form-check-input" type="radio" name="usr_right" value=\'' . $rightValue . '\'';
                         echo ($mustCheckRadio? 'checked' : '') . ' > '. $rightValue . '<br>';
                     }
@@ -84,7 +61,7 @@
             <label for="Enable">Profil actif</label>
             <div class="form-check">
             <?php
-            $mustCheckRadio = ($userToEdit->getEnable() == 1);
+            $mustCheckRadio = ($userSelected->getEnable() == 1);
 
             echo '<input class="form-check-input" type="radio" name="usr_enable" value=1 ';
             echo ($mustCheckRadio ? 'checked' : '');
