fd80d51998e51fb9766952a72e95b6d8bcaf893a
Matthieu Besson <bmatthieu.edn@gmail.com>
2018-01-11 14:45:50 +0100

    disable inputs in edit mode, date format changed

--- templates/usr_fiche_edit.php
+++ templates/usr_fiche_edit.php
@@ -20,16 +20,16 @@
     <legend>Informations d'identification</legend>
     <div class="form-group">
         <label for="Nom">Nom</label>
-        <input class="form-control" id="Nom" type="text" name="usr_name" value="<?php echo $userSelected->getName(); ?>"/>
+        <input class="form-control" id="Nom" type="text" name="usr_name" value="<?php echo $userSelected->getName(); ?>" <?php echo $modAct!=='edit' ? ' disabled' : '' ?> />
     </div>
     <div class="form-row">
         <div class="form-group col-md-6">
             <label for="Profil">Profil</label>
-            <input class="form-control" id="Profil" type="text" name="usr_login" value="<?php echo $userSelected->getLogin() ?>" <?php echo $modAct!=='edit' ? 'disabled' : '' ?>/>
+            <input class="form-control" id="Profil" type="text" name="usr_login" value="<?php echo $userSelected->getLogin() ?>" <?php echo $modAct!=='edit' ? ' disabled' : '' ?> />
         </div>
         <div class="form-group col-md-6">
             <label for="Pass">Mot de Passe</label>
-            <input type="text" class="form-control" id="Pass" name="usr_pwd" value="<?php echo $userSelected->getPassword(); ?>"/>
+            <input type="text" class="form-control" id="Pass" name="usr_pwd" value="<?php echo $userSelected->getPassword(); ?>" <?php echo $modAct!=='edit' ? ' disabled' : '' ?>/>
         </div>
     </div>
 
@@ -38,7 +38,7 @@
     <div class="form-row">
         <div class="form-group col-md-4">
             <label for="Date">Date d'inscription :</label>
-            <input type="date" class="form-control" id="Date" name="usr_create" value="<?php echo $userSelected->getCreationDateForDisplay(); ?>"/>
+            <input type="date" class="form-control" id="Date" name="usr_create" value="<?php echo $userSelected->getCreationDate(); ?>" <?php echo $modAct!=='edit' ? ' disabled' : '' ?> />
 
         </div>
         <div class="form-group col-md-4">
@@ -51,7 +51,9 @@
                 foreach(User::getRightsValues() as $rightValue){
                         $mustCheckRadio = ($rightValue == $userSelected->getRights());
                         echo '<input class="form-check-input" type="radio" name="usr_right" value=\'' . $rightValue . '\'';
-                        echo ($mustCheckRadio? 'checked' : '') . ' > '. $rightValue . '<br>';
+                        echo ($mustCheckRadio? 'checked' : '');
+                        echo $modAct!=='edit' ? ' disabled' : '';
+                        echo ' > ' . $rightValue . '<br>';
                     }
                 ?>
             </div>
@@ -65,10 +67,12 @@
 
             echo '<input class="form-check-input" type="radio" name="usr_enable" value=1 ';
             echo ($mustCheckRadio ? 'checked' : '');
+            echo $modAct!=='edit' ? ' disabled' : '';
             echo '> Oui<br>';
 
             echo '<input class="form-check-input" type="radio" name="usr_enable" value=0 ';
             echo (!$mustCheckRadio ? 'checked' : '');
+            echo $modAct!=='edit' ? ' disabled' : '';
             echo '> Non <br>';
             ?>
         </div>
